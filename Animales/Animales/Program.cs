44e1e2c8359c9e938fcf8194811824a774cebdcf
AdriaSaz <asaz.informatica@gmail.com>
2014-12-10 16:02:51 +0100

    1

---
+++ Animales/Animales/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animales
+{
+    class Program
+    {
+        Enum TipoAnimal { Gallo, Perro, Gato, Periquito }
+        static void Main()
+        {
+
+           
+
+            switch (TipoAnimal)
+            {
+                
+            }
+        }
+    }
+}
